309796a54d6abe30b73649f9224eb10260023fe2
Huzaifa-2609 <huzaifa.oct@gmail.com>
2023-05-17 02:42:13 +0500

    ui done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Singup from "./pages/Signup";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<Singup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
