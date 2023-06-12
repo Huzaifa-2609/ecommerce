07aa004b8501024aabc4a36125731750135c98ec
KhanAsadArif <onlyasadarif@gmail.com>
2023-06-13 01:34:36 +0500

    Cart design updated

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -30,7 +30,7 @@ const Cart = ({ setOpenCart }) => {
     <div className="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10">
       <div className="fixed top-0 right-0 h-full w-[80%] 800px:w-[25%] bg-white flex flex-col overflow-y-scroll justify-between shadow-sm">
         {cart && cart.length === 0 ? (
-          <div className="w-full h-screen flex items-center justify-center">
+          <div className="w-full h-screen flex items-center justify-center bg-[#11b85c]">
             <div className="flex w-full justify-end pt-5 pr-5 fixed top-3 right-3">
               <RxCross1
                 size={25}
@@ -38,20 +38,25 @@ const Cart = ({ setOpenCart }) => {
                 onClick={() => setOpenCart(false)}
               />
             </div>
-            <h5>Cart Items is empty!</h5>
+            <h5 className="text-white">Cart Items is empty!</h5>
           </div>
         ) : (
           <>
             <div>
-              <div className="flex w-full justify-end pt-5 pr-5">
-                <RxCross1
-                  size={25}
-                  className="cursor-pointer"
-                  onClick={() => setOpenCart(false)}
-                />
+              <div className=" bg-[#11b85c]">
+              <h1 className="pt-10 mt-0 text-white text-xl">MY CART</h1>
+              </div>
+              
+              <div className="flex w-full pt-0 pb-5 pr-5 bg-[#11b85c] items-start justify-end">
+                  <RxCross1
+                    size={25}
+                    className="cursor-pointer"
+                    onClick={() => setOpenCart(false)}
+                  />
+
               </div>
               {/* Item length */}
-              <div className={`${styles.noramlFlex} p-4`}>
+              <div className={`${styles.noramlFlex} p-4 ml-7`}>
                 <IoBagHandleOutline size={25} />
                 <h5 className="pl-2 text-[20px] font-[500]">
                   {cart && cart.length} items
@@ -60,7 +65,7 @@ const Cart = ({ setOpenCart }) => {
 
               {/* cart Single Items */}
               <br />
-              <div className="w-full border-t">
+              <div className="w-full border-t ml-0">
                 {cart &&
                   cart.map((i, index) => (
                     <CartSingle
@@ -73,11 +78,11 @@ const Cart = ({ setOpenCart }) => {
               </div>
             </div>
 
-            <div className="px-5 mb-3">
+            <div className="px-5 mb-2 pt-10">
               {/* checkout buttons */}
               <Link to="/checkout">
                 <div
-                  className={`h-[45px] flex items-center justify-center w-[100%] bg-[#e44343] rounded-[5px]`}
+                  className={`h-[45px] flex items-center justify-center w-[100%] bg-[#117e2c] rounded-[5px]`}
                 >
                   <h1 className="text-[#fff] text-[18px] font-[600]">
                     Checkout Now (USD${totalPrice})
@@ -113,29 +118,29 @@ const CartSingle = ({ data, quantityChangeHandler, removeFromCartHandler }) => {
   };
 
   return (
-    <div className="border-b p-4">
+    <div className="border-b p-4 ml-7">
       <div className="w-full flex items-center">
-        <div>
+        <div className="pr-5">
           <div
-            className={`bg-[#e44343] border border-[#e4434373] rounded-full w-[25px] h-[25px] ${styles.noramlFlex} justify-center cursor-pointer`}
+            className={`bg-[#03a75a] border border-[#e4434373] rounded-full w-[25px] h-[25px] ${styles.noramlFlex} justify-center cursor-pointer`}
             onClick={() => increment(data)}
           >
-            <HiPlus size={18} color="#fff" />
+            <HiPlus size={15} color="#fff" />
           </div>
-          <span className="pl-[10px]">{data.qty}</span>
+          <span className="pl-[1px] pt-[3px] pb-[3px]">{data.qty}</span>
           <div
-            className="bg-[#a7abb14f] rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer"
+            className="bg-[#b61111] rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer"
             onClick={() => decrement(data)}
           >
-            <HiOutlineMinus size={16} color="#7d879c" />
+            <HiOutlineMinus size={15} color="#fff" />
           </div>
         </div>
         <img
           src={data?.images[0]}
           alt=""
-          className="w-[130px] h-min ml-2 mr-2 rounded-[5px]"
+          className="w-[130px] h-min ml-10 mr-2 rounded-[8px]"
         />
-        <div className="pl-[5px]">
+        <div className="pl-[20px]">
           <h1>{data.name}</h1>
           <h4 className="font-[400] text-[15px] text-[#00000082]">
             ${data.discountPrice} * {value}
@@ -145,7 +150,7 @@ const CartSingle = ({ data, quantityChangeHandler, removeFromCartHandler }) => {
           </h4>
         </div>
         <RxCross1
-          className="cursor-pointer"
+          className="ml-10 bg-[#e20a0a] text-xl text-white rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer p-2"
           onClick={() => removeFromCartHandler(data)}
         />
       </div>
