26afac25c5b1244b5c63b511968a043b0b880074
KhanAsadArif <onlyasadarif@gmail.com>
2023-06-15 00:24:50 +0500

    Wishlist design updated

--- src/components/Wishlist/Wishlist.jsx
+++ src/components/Wishlist/Wishlist.jsx
@@ -28,7 +28,7 @@ const Wishlist = ({ setOpenWishlist }) => {
     <div className="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10">
       <div className="fixed top-0 right-0 h-full w-[80%] overflow-y-scroll 800px:w-[25%] bg-white flex flex-col justify-between shadow-sm">
         {wishlist && wishlist.length === 0 ? (
-          <div className="w-full h-screen flex items-center justify-center">
+          <div className="w-full h-screen flex items-center justify-center bg-[#11b85c]">
             <div className="flex w-full justify-end pt-5 pr-5 fixed top-3 right-3">
               <RxCross1
                 size={25}
@@ -36,12 +36,16 @@ const Wishlist = ({ setOpenWishlist }) => {
                 onClick={() => setOpenWishlist(false)}
               />
             </div>
-            <h5>Wishlist Items is empty!</h5>
+            <h5 className="text-white">Wishlist Items is empty!</h5>
           </div>
         ) : (
           <>
             <div>
-              <div className="flex w-full justify-end pt-5 pr-5">
+              <div className=" bg-[#11b85c]">
+              <h1 className="pt-10 mt-0 text-white text-xl">MY WISHLIST</h1>
+              </div>
+
+              <div className="flex w-full pt-0 pb-5 pr-5 bg-[#11b85c] items-start justify-end">
                 <RxCross1
                   size={25}
                   className="cursor-pointer"
@@ -49,7 +53,7 @@ const Wishlist = ({ setOpenWishlist }) => {
                 />
               </div>
               {/* Item length */}
-              <div className={`${styles.noramlFlex} p-4`}>
+              <div className={`${styles.noramlFlex} p-4 ml-7`}>
                 <AiOutlineHeart size={25} />
                 <h5 className="pl-2 text-[20px] font-[500]">
                   {wishlist && wishlist.length} items
@@ -58,7 +62,7 @@ const Wishlist = ({ setOpenWishlist }) => {
 
               {/* cart Single Items */}
               <br />
-              <div className="w-full border-t">
+              <div className="w-full border-t ml-0">
                 {wishlist &&
                   wishlist.map((i, index) => (
                     <CartSingle
@@ -82,10 +86,11 @@ const CartSingle = ({ data, removeFromWishlistHandler, addToCartHandler }) => {
   const totalPrice = data.discountPrice * value;
 
   return (
-    <div className="border-b p-4">
+    <div className="border-b p-4 ml-7">
       <div className="w-full 800px:flex items-center">
+        <div className="flex flex-row justify-between w-2/5 items-center">
         <RxCross1
-          className="cursor-pointer 800px:mb-['unset'] 800px:ml-['unset'] mb-2 ml-2"
+          className="bg-[#e20a0a] text-xl text-white rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer p-2"
           onClick={() => removeFromWishlistHandler(data)}
         />
         <img
@@ -93,6 +98,8 @@ const CartSingle = ({ data, removeFromWishlistHandler, addToCartHandler }) => {
           alt=""
           className="w-[130px] h-min ml-2 mr-2 rounded-[5px]"
         />
+        
+        
 
         <div className="pl-[5px]">
           <h1>{data.name}</h1>
@@ -103,11 +110,13 @@ const CartSingle = ({ data, removeFromWishlistHandler, addToCartHandler }) => {
         <div>
           <BsCartPlus
             size={20}
-            className="cursor-pointer"
+            className="cursor-pointer text-[#117e2c] "
             tile="Add to cart"
             onClick={() => addToCartHandler(data)}
           />
         </div>
+        </div>
+        
       </div>
     </div>
   );
